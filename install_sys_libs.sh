e044003ab402deb36d508a6a16ecfa3913b7c1de
Alex Melkonyan <sasha.melkonyan@gmail.com>
2017-04-26 11:55:21 +0200

    Add script that will run the whole setup process

---
+++ install_sys_libs.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo apt-get -y install python3-pip
+sudo apt-get -y install libcupti-dev
+sudo apt-get -y install python3-tk
+sudo apt-get -y install unzip
---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bah
+
+./intall_sys_libs.sh
+pip3 install -r requirements.txt
+wget https://www.dropbox.com/s/9ljw0v3ycsqavjr/cudnn-8.0-linux-x64-v5.1.tgz
+./cuda_install.sh
+./download_celeba.sh
+source create_vars.sh
+
