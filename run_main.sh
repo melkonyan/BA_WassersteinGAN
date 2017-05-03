cd4bfd8a36c2673a57042c1401b49faa03740c87
Alex Melkonyan <sasha.melkonyan@gmail.com>
2017-05-03 20:30:37 +0200

    Run python script without std buffering

--- run_main.sh
+++ run_main.sh
@@ -1,2 +1,2 @@
-/usr/bin/python3 main.py --logs_dir=logs/CelebA_GAN_logs/trial1/ --optimizer=RMSProp --learning_rate=5e-5 --optimizer_param=0.9 --model=0 --iterations=100000 --mode=visualize
+/usr/bin/python3 -u main.py --logs_dir=logs/CelebA_GAN_logs/trial1/ --optimizer=RMSProp --learning_rate=5e-5 --optimizer_param=0.9 --model=0 --iterations=100000 --mode=visualize
 
