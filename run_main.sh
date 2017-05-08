42836ea3c7278b76a96d483bf65d5535eb4d30c1
Alex Melkonyan <sasha.melkonyan@gmail.com>
2017-05-08 15:50:45 +0200

    Save the whole image palette

--- run_main.sh
+++ run_main.sh
@@ -1,2 +1,2 @@
-/usr/bin/python3 main.py --logs_dir=logs/cross_dis/trial7 --optimizer=RMSProp --learning_rate=5e-5 --optimizer_param=0.9 --model=1 --iterations=2 --mode=visualize --checkpoint_file=model-15000.ckpt-15000
+/usr/bin/python3 main.py --logs_dir=logs/cross_dis/trial8 --optimizer=RMSProp --learning_rate=5e-5 --optimizer_param=0.9 --model=1 --iterations=2 --mode=visualize --checkpoint_file=model-30000.ckpt-30000
 
